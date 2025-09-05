cabbdd795d4420d46fc867ac894b6350c46120d8
shrey <shreyashyambhat@gmail.com>
2025-09-05 15:13:32 -0700

    leetcode 75 - Move Zeros- Easy

---
+++ src/Level1/MoveZeros.java
@@ -0,0 +1,26 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+    public static void main(String[] args){
+        int [] nums= new int[]{0,1,0,2,0,3};
+        move(nums);
+    }
+    public static void move(int [] nums){
+        System.out.println(Arrays.toString(nums));
+        int decrement=0;
+        for(int i=0;i<nums.length;i++){
+          if(nums[i]==0){
+              decrement++;
+          }else{
+              if(decrement!=0){
+                  nums[i-decrement]=nums[i];
+                  nums[i]=0;
+              }
+
+          }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
